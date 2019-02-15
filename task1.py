139c78737d3666de01ba4a8f6982d4d716589179
SaiSowmithK <ss591748@dal.ca>
2019-02-15 10:28:09 -0800

    1.a

--- task1.py
+++ task1.py
@@ -3,12 +3,11 @@ def unique_words(file):
         allWords = [line.split(',') for line in opened_file.readlines()]
         unique_list = list() 
         for i in allWords:
-        if i in unique_list:
-                pass
-        else:
-                unique_list.append(i);
+                if i in unique_list:
+                        pass
+                else:
+                        unique_list.append(i);
         return tuple(unique_list)
 print(unique_words("book1.txt"))
 print(unique_words("book2.txt"))
 print(unique_words("book3.txt"))
-
