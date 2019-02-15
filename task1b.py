91d59f77c31b9ffbee892a5f0749f64230dd1167
SaiSowmithK <ss591748@dal.ca>
2019-02-15 10:40:10 -0800

    1.b

--- task1b.py
+++ task1b.py
@@ -1,8 +1,9 @@
-def count_the_article(l):
+def coundef count_the_article(book1):
 	mylist=["a","the","at","run","to","and","or","for","an","this"]
 	c=0
-	for word in l:
+	for word in book1:
 		if word in mylist:
 			c=c+1
 		else:
-			
+			t_the_article(book1):
+	
