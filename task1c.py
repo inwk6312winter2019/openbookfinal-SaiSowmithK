439ed100b1fd1b364cfa0f386eac5d4ab90eb95b
SaiSowmithK <ss591748@dal.ca>
2019-02-15 10:58:22 -0800

    1.c

--- task1c.py
+++ task1c.py
@@ -1,3 +1,17 @@
-def sorted_words(book1): 
-	book1.sort(key=len) 
-	return book1 
+import string
+
+def sorted_words():
+ 
+ reserve = []
+        f = open('Book1.txt', 'r')
+        w = f.readlines()
+
+        for line in w:
+                res = line.lower().strip(string.punctuation + string.whitespace$
+                reserve = reserve + res
+
+        words = my_str.split()
+        words.sort()
+        print("The sorted words are:")
+        for word in w:
+sorted_words(word)
