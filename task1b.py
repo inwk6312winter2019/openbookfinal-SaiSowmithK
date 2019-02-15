81336d76d8d1a409244f29d5d9e8be3357c734e2
SaiSowmithK <ss591748@dal.ca>
2019-02-15 10:56:33 -0800

    1.b

--- task1b.py
+++ task1b.py
@@ -1,9 +1,16 @@
-def coundef count_the_article(book1):
-	mylist=["a","the","at","run","to","and","or","for","an","this"]
-	c=0
-	for word in book1:
-		if word in mylist:
-			c=c+1
-		else:
-			t_the_article(book1):
-	
+def count_the_article():
+	reserve = []
+	f = open('Book1.txt', 'r')
+	w = f.readlines()
+
+	for line in w:
+		res = line.lower().strip(string.punctuation + string.whitespace).split()
+		reserve = reserve + res
+
+	count = {}
+        for count, word in w:
+		print(word + " : " + str(concat))
+
+	print("Words " + str(total) + "\n" + "Different words: " + str(diff_word) + "\n" + "Vocab ratio: " + str(vocab))
+
+count_the_article()
